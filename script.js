404b17376c523a60b1a75f1a01135713895a6163
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-07-31 15:53:16 +0530

    Javascript : added delete button in li element, and insert before element showed 1st index in the Do-To-List

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-const addItem = () =>{
+const addItem = () => {
     let inputValue = document.getElementById("tbox-name").value;
     console.log(inputValue);
 
     let box = document.getElementById("box");
 
     let li = document.createElement('li');
-    li.textContent= inputValue;
+    li.textContent = inputValue;
 
-    box.appendChild(li)
+
+    let a = document.createElement('a');
+    a.textContent = "X";
+    let aStl = a.style
+    aStl.textDecoration = 'none';
+    aStl.color = "red";
+    aStl.cursor = "pointer";
+    aStl.padding = '8px';
+    li.appendChild(a)
+
+
+    let insertBefore = box.firstElementChild;
+    if(insertBefore == null){
+        box.appendChild(li);
+    }else{
+        box.insertBefore(li, insertBefore)
+    }
+
 }
 
